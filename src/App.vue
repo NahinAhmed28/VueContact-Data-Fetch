a63fc82ea8663b6ed14d9ff19c02d070c7ada93b
NahinAhmed28 <72101670+NahinAhmed28@users.noreply.github.com>
2021-08-21 04:45:45 +0600

    file upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+<button class="btn btn-primary" @click="componentToRender='compHome'" >Home </button>
+<button  class="btn btn-outline-info" @click="componentToRender='compContact'" >Contact </button>
+<button class="btn btn-outline-success" @click="componentToRender='compAbout'" >About </button>
+
+
+<keep-alive>
+<component :is="componentToRender"></component>
+   </keep-alive>
+    </div>
+</template>
+
+<script>
+import compHome from './Components/home'
+import compAbout from './Components/about'
+import compContact from './Components/contact'
+
+export default{
+    data() {
+        return{
+componentToRender:'compHome'
+        }
+    },
+    components: {
+        compHome,
+        compAbout,
+        compContact
+    }
+
+
+}
+</script>
---
+++ src/Components/home.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        
+
+
+    <div class="w3-content" style="max-width:2000px; ">
+
+        <img class="w3-image" src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=902&q=80" alt="Hamburger Catering" width="500px" height="300px" float:left>
+    
+
+        
+        <img class="w3-image" src="https://media.architecturaldigest.com/photos/582cce93efb84d105bd02260/4:3/w_1439,h_1080,c_limit/place-setting.jpg"  alt="Table Setting" width="500px" height="300px" >
+       
+
+    
+    </div>
+  
+  <hr>
+  
+  <!-- Menu Section -->
+  <div class="w3-row w3-padding-64" id="menu">
+    <div class="w3-col l6 w3-padding-large">
+      <h1 class="w3-center">Our Menu</h1><br>
+      <h4>Bread Basket</h4>
+      <p class="w3-text-grey">Assortment of fresh baked fruit breads and muffins 5.50</p><br>
+    
+      <h4>Honey Almond Granola with Fruits</h4>
+      <p class="w3-text-grey">Natural cereal of honey toasted oats, raisins, almonds and dates 7.00</p><br>
+    
+      <h4>Belgian Waffle</h4>
+      <p class="w3-text-grey">Vanilla flavored batter with malted flour 7.50</p><br>
+    
+      <h4>Scrambled eggs</h4>
+      <p class="w3-text-grey">Scrambled eggs, roasted red pepper and garlic, with green onions 7.50</p><br>
+    
+      <h4>Blueberry Pancakes</h4>
+      <p class="w3-text-grey">With syrup, butter and lots of berries 8.50</p>    
+    </div>
+    
+    <div class="w3-col l6 w3-padding-large">
+      <img src="https://png.pngtree.com/thumb_back/fw800/back_pic/04/15/58/005822da6221b4c.jpg" class="w3-round w3-image w3-opacity-min" alt="Menu" style="width:100%">
+    </div>
+  </div>
+
+  <hr>
+
+</div>
+
+
+
+
+   
+</template>
+
+<style>
+body {font-family: "Times New Roman", Georgia, Serif;}
+h1, h2, h3, h4, h5, h6 {
+  font-family: "Playfair Display";
+  letter-spacing: 5px;
+}
+div.w3-content{
+    display: inline-block;
+    margin-left: 10px;
+    padding-top: 20px;
+}
+
+</style>
+
